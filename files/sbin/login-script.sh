60643b48d8ad24136a40b8e2a781e73f47c85ea6
root <root@server>
2016-10-20 17:32:30 +0200

    Startseite Browser neu, cron-Jobs, popup-download-helper

--- files/sbin/login-script.sh
+++ files/sbin/login-script.sh
@@ -5,14 +5,15 @@ while [ ! -e /nwbox/public/ibinda.public ]
 do
   sleep 1
   timeout1=$(($timeout1+1))
-  if [ $timeout1 -ge 120 ]
+  if [ $timeout1 -ge 600 ]
   then
-    echo "no net" > /var/log/timeout-net.log
+    date > /var/log/timeout-net1.log
+    echo "no net" >> /var/log/timeout-net1.log
     exit 0
   fi
 done
 
-date > /var/log/time-login.log
-echo $timeout1 >> /var/log/time-login.log
+date > /var/log/time-login1.log
+echo $timeout1 >> /var/log/time-login1.log
 
 exit 0
